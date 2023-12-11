942efa98f4f46bb972e1edd710af8558da301d9e
Wither19 <146484561+Wither19@users.noreply.github.com>
2023-12-11 11:28:14 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,11 @@ function resized() {
   document.getElementById('w').innerHTML = window.innerWidth;
   document.getElementById('h').innerHTML = window.innerHeight;
 }
+function copyStyles() {
+            navigator.clipboard.writeText(&ltlink rel="stylesheet" href="https://wither19.github.io/demoknightcss/style.css"&gt);
+        }
+function copyScripts() {
+            navigator.clipboard.writeText(&ltscript src="https://wither19.github.io/demoknightcss/style.css"&gt&lt/script&gt);
+        }
+document.getElementById('copy-css').addEventListener("click", copyStyles);
+document.getElementById('copy-js').addEventListener("click", copyScripts);
